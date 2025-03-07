80169725c3fae26cac1639f4da0790db4ddb2cae
imjusthereforminecraft <gcouso717@gmail.com>
2025-03-07 08:51:51 -0500

    unused import cleanup in LedSubsystem

--- src/main/java/frc/robot/Subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/Subsystems/LedSubsystem.java
@@ -1,23 +1,13 @@
 package frc.robot.Subsystems;
 
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-
 import java.util.ArrayList;
 
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedSubsystem extends SubsystemBase {
-
-    private final Distance kLedSpacing = Meters.of(1 / 120.0);
-    private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
-    private final LEDPattern m_scrollingRainbow =
-        m_rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
 
     private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(70);
     private final AddressableLED led = new AddressableLED(0);
